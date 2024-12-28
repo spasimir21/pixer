6b475a481432a7b955d6a41697908e249aecc428
Spasimir Pavlov <spasimir.pavlov@gmail.com>
2024-12-28 19:19:17 +0200

    Fixed build issues

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,4 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+exports.plugins = {
+  tailwindcss: {},
+  autoprefixer: {}
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import viteTypescript from './lib/vite-plugin-typescript';
-import tsconfigPaths from 'vite-tsconfig-paths';
 import typescript from 'ts-patch/compiler';
 import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [viteTypescript([path.join(__dirname, './src/main.ts')], typescript), tsconfigPaths()]
+  plugins: [viteTypescript([path.join(__dirname, './src/main.ts')], typescript)]
 });
